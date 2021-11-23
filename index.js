e4085331d6aaa3bc66d88ba9c1993a29c12fc361
josephdaw <josephdaw@icloud.com>
2021-11-23 22:58:48 +1030

    adding initial inquirer prompt

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const inquirer = require("inquirer");
+
+const init = async () => {
+    const choice = await inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: 'action',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update an Employee'
+            ]
+        }
+    ])
+
+    // if (choice.role === 'Engineer') { await this.addEngineer() }
+    if (choice.action === 'View All Departments') { console.log("1") }
+    if (choice.action === 'View All Roles') { console.log("2") }
+    if (choice.action === 'View All Employees') { console.log("3") }
+    if (choice.action === 'Add a Department') { console.log("4") }
+    if (choice.action === 'Add a Role') { console.log("5") }
+    if (choice.action === 'Add an Employee') { console.log("6") }
+    if (choice.action === 'Update an Employee') { console.log("7") }
+}
+
+init();

0d18a20320df103b6982abbfb9cc4a365ded8291
josephdaw <josephdaw@icloud.com>
2021-11-24 10:38:21 +1030

    adding view all departments query function

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 require('dotenv').config();
 
 const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+const department = require('./lib/department')
+
 
 const init = async () => {
     const choice = await inquirer
-    .prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            name: 'action',
-            choices: [
-                'View All Departments',
-                'View All Roles',
-                'View All Employees',
-                'Add a Department',
-                'Add a Role',
-                'Add an Employee',
-                'Update an Employee',
-                'Exit'
-            ]
-        }
-    ])
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: 'action',
+                choices: [
+                    'View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add a Department',
+                    'Add a Role',
+                    'Add an Employee',
+                    'Update an Employee',
+                    'Exit'
+                ]
+            }
+        ])
 
     // if (choice.role === 'Engineer') { await this.addEngineer() }
-    if (choice.action === 'View All Departments') { console.log("1") }
+    if (choice.action === 'View All Departments') {
+        await department.viewAll()
+        init()
+    }
+
     if (choice.action === 'View All Roles') { console.log("2") }
     if (choice.action === 'View All Employees') { console.log("3") }
     if (choice.action === 'Add a Department') { console.log("4") }
@@ -32,5 +40,6 @@ const init = async () => {
     if (choice.action === 'Update an Employee') { console.log("7") }
     if (choice.action === 'Exit') { return console.log("Logged Out") }
 }
+
 
 init();
